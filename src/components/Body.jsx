7b8575416db715d0c5f13d234d8052837e41dcdd
Adarsh Jha <jha01adarsh@gmail.com>
2026-05-30 11:36:42 +0530

    Add event - correct & wrong way

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,6 +6,16 @@ const Body = () => (
     <div id='body-search-bar'>
       <input id='input-txt' type='text'></input>
       <button type='submit'>Search</button>
+      <button
+        id='top-rated-res-btn'
+        type='submit'
+        // onMouseOver={console.log('Hello')} // wrong
+        onClick={() => {
+          console.log('Hello');
+        }}
+      >
+        Top Rated Restaurants
+      </button>
     </div>
     <div id='body-res-container'>
       {resData.map((restaurant, index) => (
