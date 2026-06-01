297524bad28e272d40a6c5c5e8878d91876dc68c
Adarsh Jha <jha01adarsh@gmail.com>
2026-06-01 22:17:37 +0530

    Mock data rendered

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,60 +1,10 @@
-// import { resData } from '../utilities/mockData.js';
+import { restaurantData } from '../utilities/mockData.js';
 import { useState } from 'react';
 import RestaurantCard from '../components/RestaurantCard';
 
 const Body = () => {
   // use state variable
-  let [resData, setResData] = useState([
-    {
-      card: {
-        card: {
-          info: {
-            id: '305807',
-            name: 'Barbeque Nation',
-            cloudinaryImageId: 'pocpd4knzgptsuguulhv',
-            cuisines: [
-              'North Indian',
-              'Barbecue',
-              'Kebabs',
-              'Biryani',
-              'Street Food',
-              'Snacks',
-            ],
-            avgRating: 4.2,
-            eta: 43,
-          },
-        },
-      },
-    },
-    {
-      card: {
-        card: {
-          info: {
-            id: '305808',
-            name: 'Taj Restaurant',
-            cloudinaryImageId: 'pocpd4knzgptsuguulhv',
-            cuisines: ['North Indian', 'Mughlai', 'Biryani'],
-            avgRating: 4.5,
-            eta: 35,
-          },
-        },
-      },
-    },
-    {
-      card: {
-        card: {
-          info: {
-            id: '305808',
-            name: 'Taj Restaurant',
-            cloudinaryImageId: 'pocpd4knzgptsuguulhv',
-            cuisines: ['North Indian', 'Mughlai', 'Biryani'],
-            avgRating: 3.6,
-            eta: 35,
-          },
-        },
-      },
-    },
-  ]);
+  const [resData, setResData] = useState(restaurantData);
 
   return (
     <div id='main-body'>
@@ -66,7 +16,7 @@ const Body = () => {
           type='submit'
           onClick={() => {
             let filteredResData = resData.filter((resData) => {
-              return resData.card.card.info.avgRating > 4;
+              return resData.card.card.info.avgRating >= 4.5;
             });
             setResData(filteredResData);
           }}
--- src/utilities/mockData.js
+++ src/utilities/mockData.js
@@ -1,4 +1,4 @@
-export const resData = [
+export const restaurantData = [
   {
     card: {
       card: {
