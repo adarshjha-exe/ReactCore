790088dcd9ae2b097ddd965b887c7c0e24f61f52
Adarsh Jha <jha01adarsh@gmail.com>
2026-06-02 23:34:46 +0530

    UseState - Login/Logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,33 @@
+import { useState } from 'react';
 import { APP_LOGO } from '../utilities/mockData';
 
-const Header = () => (
-  <div className='header-div'>
-    <div className='logo'>
-      <img src={APP_LOGO} alt='logo' />
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div className='header-div'>
+      <div className='logo'>
+        <img src={APP_LOGO} alt='logo' />
+      </div>
+      <div className='nav-tags'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <li>
+            <button
+              className='login-btn'
+              type='submit'
+              onClick={() => {
+                setIsLoggedIn(!isLoggedIn);
+              }}
+            >
+              {isLoggedIn ? 'Logout' : 'Login'}
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div className='nav-tags'>
-      <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
-);
-
+  );
+};
 export default Header;
