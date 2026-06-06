89618b961881eb78554669431d5d633f929290f4
Adarsh Jha <jha01adarsh@gmail.com>
2026-06-06 23:39:48 +0530

    error handling in react router

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,14 @@
+import { useRouteError } from 'react-router-dom';
+
 const Error = () => {
+  const error = useRouteError();
+  console.log(error);
   return (
     <div>
       <h1>Something went wrong....</h1>
+      <p>
+        {error.status} - {error.statusText}
+      </p>
     </div>
   );
 };
