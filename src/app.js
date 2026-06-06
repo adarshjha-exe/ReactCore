df8bc705800d5e47ca94d2ff8af0d3a63db32203
Adarsh Jha <jha01adarsh@gmail.com>
2026-06-06 18:02:35 +0530

    Routes + Error handling in routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { About } from './components/About.jsx';
+import Error from './components/Error.jsx';
 
 const AppLayout = () => {
   return (
@@ -11,5 +14,17 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+export const About = () => {
+  return (
+    <div>
+      <h1>Welcome to the About Page</h1>
+      <h2>About Us</h2>
+    </div>
+  );
+};
