1e17df86edb8a96b6ac3000abe5eb7809c6244dc
Adarsh Jha <jha01adarsh@gmail.com>
2026-06-08 13:05:12 +0530

    CLASS : useState vaiable

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 const User = (props) => {
   const { name, location } = props;
+  // state variable
+  const [count1] = useState(0);
+  const [count2] = useState(2);
   return (
     <div className='user-div'>
+      <h2>Count1 func :{count1} </h2>
+      <h2>Count2 func : {count2}</h2>
       <h2>Name : {name}</h2>
       <h3>Location : {location}</h3>
       <h3>Contact : @adarshjha-exe</h3>
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -3,13 +3,21 @@ import React from 'react';
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    // state varibale( object)
+    this.state = {
+      count3: 3,
+      count4: 4,
+    };
   }
 
   // render method which return the piece of JSX
   render() {
     const { username } = this.props;
+    const { count4 } = this.state;
     return (
       <div className='user-div'>
+        <h2>Count3 class : {this.state.count3}</h2>
+        <h2>Count4 class : {count4}</h2>
         <h2>Name : {this.props.name}</h2>
         <h3>Location : BLR</h3>
         <h3>Contact : {username}</h3>
