6c4fdc999e199f3ed04ec118fe09720cda9bc2cd
Adarsh Jha <jha01adarsh@gmail.com>
2026-06-08 15:56:37 +0530

    CLASS : update state variable

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -14,10 +14,21 @@ class UserClass extends React.Component {
   render() {
     const { username } = this.props;
     const { count4 } = this.state;
+
+    // update the state variable
     return (
       <div className='user-div'>
-        <h2>Count3 class : {this.state.count3}</h2>
         <h2>Count4 class : {count4}</h2>
+        <button
+          onClick={() => {
+            this.setState({
+              count4: this.state.count4 + 1,
+              count3: this.state.count3 + 1,
+            });
+          }}
+        >
+          Increase Count
+        </button>
         <h2>Name : {this.props.name}</h2>
         <h3>Location : BLR</h3>
         <h3>Contact : {username}</h3>
