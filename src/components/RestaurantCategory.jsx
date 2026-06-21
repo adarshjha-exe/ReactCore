06f340fb41d92e7b1e6701fae6ca0ad37ebb7cb3
Adarsh Jha <jha01adarsh@gmail.com>
2026-06-21 16:43:35 +0530

    Menu show/hide Accordian

--- src/components/RestaurantCategory.jsx
+++ src/components/RestaurantCategory.jsx
@@ -1,19 +1,26 @@
+import { useState } from 'react';
 import { ItemList } from './ItemList';
 
 export const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItems] = useState(false);
+
+  const handleClick = () => {
+    setShowItems(!showItems);
+  };
+
   return (
     <div className='category-section'>
       {/* Header */}
-      <div className='category-header'>
+      <div className='category-header' onClick={handleClick}>
         <span className='category-title'>
           {data.title}{' '}
           <span className='item-count'>({data.itemCards.length})</span>
         </span>
         <span className='dropdown-icon'>🔽</span>
       </div>
-      {/* Content */}
+      {/* Content - on click then Items should hide or show */}
       <div className='category-items'>
-        <ItemList items={data} />
+        {showItems && <ItemList items={data} />}
       </div>
     </div>
   );
