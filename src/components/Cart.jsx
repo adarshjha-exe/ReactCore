00dcb8c9b057e29bbab56a758e05b26cde7682b7
Adarsh Jha <jha01adarsh@gmail.com>
2026-08-10 10:14:41 +0530

    show cart data on cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,16 @@
+import { useSelector } from 'react-redux';
+import { ItemList } from './ItemList';
+
 const Cart = () => {
+  // I want cart items here, so we need to subscribe to the store
+
+  const cartItems = useSelector((store) => store.cart.items);
+  console.log(cartItems);
+
   return (
     <div>
       <h1>Cart</h1>
+      <ItemList items={cartItems} />
     </div>
   );
 };
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,12 +4,12 @@ import { addItem } from '../utilities/cartSlice.js';
 export const ItemList = ({ items, dummy }) => {
   const dispatch = useDispatch();
 
-  const handleAddItem = () => {
+  const handleAddItem = (items) => {
     // dispatch an action
-    dispatch(addItem('pizza')); // pizza will be action.payload in obj format
+    dispatch(addItem(items));
   };
 
-  const itemCards = items?.itemCards;
+  const itemCards = items?.itemCards || items; // hack (ignore it)
   return itemCards.map((items, index) => {
     const price =
       items.card.info.price / 100 || items.card.info.defaultPrice / 100;
@@ -19,7 +19,7 @@ export const ItemList = ({ items, dummy }) => {
           <h3 className='item-name'>{items.card.info.name}</h3>
           <div className='item-actions'>
             <span className='item-price'>₹{price}</span>
-            <button className='add-button' onClick={handleAddItem}>
+            <button className='add-button' onClick={() => handleAddItem(items)}>
               Add +
             </button>
           </div>
