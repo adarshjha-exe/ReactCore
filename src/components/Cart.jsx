89c854e0b7aed34407da5236d557900c17e3cdc8
Adarsh Jha <jha01adarsh@gmail.com>
2026-08-10 10:25:16 +0530

    clear cart feature

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ItemList } from './ItemList';
+import { clearItems } from '../utilities/cartSlice';
 
 const Cart = () => {
   // I want cart items here, so we need to subscribe to the store
@@ -7,10 +8,23 @@ const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
   console.log(cartItems);
 
+  const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    // on button click dispatch an action
+    dispatch(clearItems());
+  };
+
   return (
     <div>
       <h1>Cart</h1>
+      <button type='button' onClick={() => handleClearCart()}>
+        Clear Cart
+      </button>
       <ItemList items={cartItems} />
+      {cartItems.length === 0 && (
+        <h1>Cart is Empty,  Add items to your cart </h1>
+      )}
     </div>
   );
 };
